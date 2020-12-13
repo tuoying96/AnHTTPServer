11a9b712f3b341099a8649122eb49587a7750046
MengxiaoHuang <62216889+MengxiaoHuang@users.noreply.github.com>
2020-12-12 23:05:39 -0800

    Add files via upload

--- http_src/http_methods.c
+++ http_src/http_methods.c
@@ -29,6 +29,104 @@
 #include "file_util.h"
 
 /**
+ * Generate html content for directory index page.
+ *
+ * @param path the directory path
+ * @param buf the string buffer to hold html content
+ */
+static void dir_content(const char *path, char *buf) {
+    sprintf(buf, "<html>\n"
+                 "<head>\n"
+                 "  <title>index of %s</title>\n"
+                 "</head>\n"
+                 "<body>\n"
+                 "  <h1>Index of %s</h1>\n"
+                 "  <table>\n"
+                 "  <tr>\n"
+                 "    <th valign=\"top\"></th>\n"
+                 "    <th>Name</th>\n"
+                 "    <th>Last modified</th>\n"
+                 "    <th>Size</th>\n"
+                 "    <th>Description</th>\n"
+                 "  </tr>\n"
+                 "  <tr>\n"
+                 "    <td colspan=\"5\"><hr></td>\n"
+                 "  </tr>", path, path); // TODO: remove the top directory
+    DIR *dp = opendir(path);
+    struct dirent *ep;
+    while (ep = readdir (dp)) {
+        if (strcmp(ep->d_name, ".") == 0) {
+            continue;
+        }
+        char name[MAXBUF], link[MAXBUF], mtime[MAXBUF], td[MAXBUF], size[MAXBUF];
+        if (ep->d_type == DT_DIR) {
+            sprintf(link, "%s/", ep->d_name);
+            if (strcmp(ep->d_name, "..")==0) {
+                if (strcmp(path, "content/")==0) {
+                    continue;
+                } else {
+                    sprintf(td, "&#x23ce");
+                    sprintf(name, "Parent Directory");
+                }
+            } else {
+                sprintf(td, "&#x1F4C1;");
+                sprintf(name, ep->d_name);
+            }
+        } else {
+            sprintf(name, ep->d_name);
+            sprintf(link, ep->d_name);
+            sprintf(td, "");
+        }
+        char fullPath[MAXBUF];
+        sprintf(fullPath, "%s%s", path, ep->d_name);
+        struct stat sb;
+        stat(fullPath, &sb);
+        time_t timer = sb.st_mtim.tv_sec;
+        milliTimeToRFC_1123_Date_Time(timer, mtime);
+        sprintf(size,"%lu",(size_t)sb.st_size);
+        sprintf(buf+strlen(buf), "  <tr>\n"
+                                 "    <td>%s</td>\n"
+                                 "    <td><a href=\"%s\">%s</a></td>\n"
+                                 "    <td align=\"right\">%s</td>\n"
+                                 "    <td align=\"right\">%s</td>\n"
+                                 "    <td></td>\n"
+                                 "  </tr>\n",td, link, name, mtime, size);
+    }
+    (void) closedir (dp);
+    sprintf(buf+strlen(buf), "  <tr><td colspan=\"5\"><hr></td></tr>\n"
+                             "</body>\n"
+                             "</html>");
+}
+
+/**
+ * Handle GET or HEAD request for directory.
+ *
+ * @param stream the socket stream
+ * @param path the directory path
+ * @param requestHeaders the request headers
+ * @param responseHeaders the response headers
+ * @param sendContent send content (GET)
+ */
+static void do_dir(FILE *stream, const char *path, Properties *requestHeaders, Properties *responseHeaders, bool sendContent) {
+    char buf[2048];
+    dir_content(path, buf);
+    // Put content to tmp file and get file stat.
+    FILE *tmp = tmpStringFile(buf);
+    char lenBuf[MAXBUF];
+    sprintf(lenBuf,"%lu", strlen(buf));
+    putProperty(responseHeaders,"Content-Length", lenBuf);
+    putProperty(responseHeaders, "Content-type", "text/html");
+    // send response
+    sendResponseStatus(stream, Http_OK, NULL);
+    // Send response headers
+    sendResponseHeaders(stream, responseHeaders);
+    if (sendContent) {  // for GET
+        copyFileStreamBytes(tmp, stream, strlen(buf));
+        fclose(tmp);
+    }
+}
+
+/**
  * Handle GET or HEAD request.
  *
  * @param stream the socket stream
@@ -52,7 +150,7 @@ static void do_get_or_head(FILE *stream, const char *uri, Properties *requestHea
 	// directory path ends with '/'
 	if (S_ISDIR(sb.st_mode) && strendswith(filePath, "/")) {
 		// not allowed for this method
-		sendStatusResponse(stream, Http_MethodNotAllowed, NULL, responseHeaders);
+		do_dir(stream, filePath, requestHeaders, responseHeaders, sendContent);
 		return;
 	} else if (!S_ISREG(sb.st_mode)) { // error if not regular file
 		sendStatusResponse(stream, Http_NotFound, NULL, responseHeaders);
